be5a807bf4d81e4bbcd52401f00bf39c5afb7313
CODEWIZARD <oyefolutoyyib16@gmail.com>
2025-03-01 00:24:48 +0100

    08-fetch-data

--- src/starter/08-fetch-data/index.tsx
+++ src/starter/08-fetch-data/index.tsx
@@ -1,4 +1,31 @@
+import { useState, useEffect } from "react";
+const url = "https://www.course-api.com/react-tours-project";
+
 function Component() {
+  // tours
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(`Failed to fetch tours...`);
+
+        const rawData = await response.json();
+        console.log(rawData);
+      } catch (error) {
+        const message =
+          error instanceof Error ? error.message : "there was an error...";
+        setIsError(message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h2>React & Typescript</h2>
